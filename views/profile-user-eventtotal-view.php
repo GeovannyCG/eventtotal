785fc9c3a9e3fc9303e7208e651c64c7f694075a
Jonathan_Leon <138831519+JonathanLeon11@users.noreply.github.com>
2023-07-27 19:33:39 -0600

    Update profile-user-eventtotal-view.php

--- views/profile-user-eventtotal-view.php
+++ views/profile-user-eventtotal-view.php
@@ -159,7 +159,7 @@
             </li>
 
             <li>
-                <a href="./supplies-eventease.html">
+                <a href="../Insumos/">
                     <i class='bx bxs-baguette'></i>
                     <span>Insumos</span>
                 </a>
