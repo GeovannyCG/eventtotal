4a31b1d7d4e2aceb838ad6a84a8c157c160e4232
Jonathan_Leon <138831519+JonathanLeon11@users.noreply.github.com>
2023-07-27 19:36:04 -0600

    Add files via upload

---
+++ models/insumos-eventtotal-model.php
@@ -0,0 +1,43 @@
+<?php
+
+session_start();
+
+require('../config/database.php');
+
+class Insumo {
+    private $connect;
+
+    public function __construct() {
+        $connectObject = new Connection();
+        $this->connect = $connectObject->getConn();
+    }
+
+    //Funcion para obtener todos los insumos de la base de datos
+    public function get_Insumos(){
+        $consulta_get_Insumos = ("SELECT * FROM insumos_eventtotal");
+        $execute_consulta_get_Insumos = $this->connect->query($consulta_get_Insumos);
+        $insumos = array();
+        while ($fila1 = $execute_consulta_get_Insumos->fetch_assoc()) {
+            $insumos[] = $fila1;
+        }
+        return $insumos;
+    }
+
+    //Funcion para insertar nuevos usuarios
+    public function insert_Insumos($nombre_is, $categoria_is, $unidad_medida_is, $cantidad, $precio, $descripcion){
+        $consulta_insert_Insumos = ("INSERT INTO insumos_eventtotal SET nombre_is = '$nombre_is', categoria_is = '$categoria_is', unidad_medida_is = '$unidad_medida_is', cantidad_is = '$cantidad', precio_unitario_is = '$precio', descripcion_is = '$descripcion'");
+        $execute_consulta_insert_Insumos = $this->connect->query($consulta_insert_Insumos);
+    }
+
+    //Funcion para actualizar usuarios
+    public function update_Insumos($id,$nombre_is, $categoria_is, $unidad_medida_is, $cantidad, $precio, $descripcion){
+        $consulta_update_Users = "UPDATE insumos_eventtotal SET nombre_is = '$nombre_is', categoria_is = '$categoria_is', unidad_medida_is = '$unidad_medida_is', cantidad_is = '$cantidad', precio_unitario_is = '$precio', descripcion_is = '$descripcion' WHERE id_is = '$id'";
+        $execute_consulta_update_Users = $this->connect->query($consulta_update_Users);
+    }
+
+    //Funcion para borrar usuarios
+    public function delete_Insumos($id){
+        $consulta_delete_Users = "DELETE FROM insumos_eventtotal WHERE id_is = $id";
+        $query4 = mysqli_query($this->connect, $consulta_delete_Users);
+    }
+}
