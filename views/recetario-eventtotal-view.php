b89e2719cef0efaca763297ecf35ff91ffd88388
Jonathan_Leon <138831519+JonathanLeon11@users.noreply.github.com>
2023-07-27 19:34:33 -0600

    Update recetario-eventtotal-view.php

--- views/recetario-eventtotal-view.php
+++ views/recetario-eventtotal-view.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>EventTotal | Users</title>
+    <title>EventTotal | Recetario</title>
     <!-- link de favicon -->
     <link rel="shortcut icon" href="../assets/images/logotipo-main-curt.ico" type="image/x-icon">
     <!-- link css de boostrap -->
