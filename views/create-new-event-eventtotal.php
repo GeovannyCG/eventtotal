2edd293ede5ad76b97f6c1247914c8274355d5bd
Goos1508 <115092436+GustavoMontiel15@users.noreply.github.com>
2023-07-27 23:02:43 -0600

    Update create-new-event-eventtotal.php

--- views/create-new-event-eventtotal.php
+++ views/create-new-event-eventtotal.php
@@ -148,7 +148,7 @@
             </li>
 
             <li>
-                <a href="./inventory-eventease.html">
+                <a href="../Inventory/">
                     <i class='bx bxs-cube'></i>
                     <span>Inventario</span>
                 </a>
