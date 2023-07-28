39b69a14c6472053429a34a8a392b5b673b023c1
Jonathan_Leon <138831519+JonathanLeon11@users.noreply.github.com>
2023-07-27 18:57:06 -0600

    Update users-eventtotal-view.php

--- views/users-eventtotal-view.php
+++ views/users-eventtotal-view.php
@@ -179,7 +179,7 @@
                 <div class="page-title-box d-sm-flex align-items-center justify-content-between">
                     <div class="page-title-right">
                         <ol class="breadcrumb m-0">
-                            <li class="breadcrumb-item active">Modulo de Usuarios</li>
+                            <li class="breadcrumb-item active">Usuarios</li>
                         </ol>
                     </div>
                 </div>
