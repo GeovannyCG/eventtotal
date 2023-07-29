29a5b6a12d7337ba624d77e813903b0ddc159d28
Goos1508 <115092436+GustavoMontiel15@users.noreply.github.com>
2023-07-28 19:26:00 -0600

    Add files via upload

---
+++ controllers/salones-eventtotal-controller.php
@@ -0,0 +1,20 @@
+<?php
+require('../Salones/');
+
+// Verificar si el usuario ha iniciado sesión
+if (isset($_SESSION['email'])) {
+    try {
+        $salones_Object = new SalonesEventTotal();
+
+        $get_salones = $salones_Object->get_Salones();
+
+    } catch (\Throwable $th) {
+        // En caso de que ocurra un error, redirecciona al usuario a la página de error interno.
+        echo "<script>location.href='../Error-Internal/'</script>";
+    }
+    // Requiere el archivo que contiene la vista "salones-eventtotal-view.php" para mostrar los resultados obtenidos.
+    require('../views/salones-eventtotal-view.php');
+} else {
+    // Si no existe una sesión activa o el valor de 'email' no está establecido, redirecciona al usuario a la página principal.
+    echo "<script>location.href='../'</script>";
+}
