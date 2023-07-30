b05daa26db62936cd09cadacc1f70695d13e7b2e
Jonathan_Leon <138831519+JonathanLeon11@users.noreply.github.com>
2023-07-30 01:34:15 -0600

    Add files via upload

--- controllers/salones-eventtotal-controller.php
+++ controllers/salones-eventtotal-controller.php
@@ -1,18 +1,21 @@
 <?php
-require('../Salones/');
+
+require('../models/salones-eventtotal-model.php');
 
 // Verificar si el usuario ha iniciado sesión
-if (isset($_SESSION['email'])) {
+if (isset($_SESSION['email'])){
     try {
-        $salones_Object = new SalonesEventTotal();
+        $salones_Object = new salones();
 
-        $get_salones = $salones_Object->get_Salones();
+        $get_salones = $salones_Object->get_salones();
+
+        $getId_Users = $salones_Object->get_Id_Users();
 
     } catch (\Throwable $th) {
         // En caso de que ocurra un error, redirecciona al usuario a la página de error interno.
-        echo "<script>location.href='../Error-Internal/'</script>";
+        echo "<script>location.href='../Error-Internal/'<script>";
     }
-    // Requiere el archivo que contiene la vista "salones-eventtotal-view.php" para mostrar los resultados obtenidos.
+    // Requiere el archivo que contiene la vista "home-eventtotal-view.php" para mostrar los resultados obtenidos.
     require('../views/salones-eventtotal-view.php');
 } else {
     // Si no existe una sesión activa o el valor de 'email' no está establecido, redirecciona al usuario a la página principal.
