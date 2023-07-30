f2b871564a66a7490d362d0ac803eac946dd1323
Jonathan_Leon <138831519+JonathanLeon11@users.noreply.github.com>
2023-07-30 01:36:06 -0600

    Add files via upload

--- actions/salones-eventtotal-action.php
+++ actions/salones-eventtotal-action.php
@@ -1,46 +1,51 @@
 <?php
-// Importa la clase SalonesEventTotal
-require('../Salones/');
 
-$salones_Obj = new SalonesEventTotal();
+require('../models/salones-eventtotal-model.php');
 
-// Verificar si el usuario ha iniciado sesión y la sesión está activa
-if (isset($_SESSION['email']) && $_SESSION['email'] !== '') {
+$salones_Object = new salones();
+
+// Verificar si el usuario ha iniciado sesión
+if ($_SESSION['email']) {
     try {
         // Comprobar si se hizo clic en el botón "Create"
         if (isset($_POST['create'])) {
-            // Manejar la creación del salón de eventos
-            $nombre_s = $_POST['Nombre_Salon'];
-            $cap_personas_s = $_POST['Capacidad_Personas'];
-            $estado_s = $_POST['Estado'];
-            $imagenes_s = $_POST['Imagenes'];
+            // Manejar la creación del usuario
+            $nombre_s = $_POST['Nombre_s'];
+            $cap_personas_s = $_POST['cap_personas_s'];
+            $estado_s = $_POST['estado_s'];
+            $imagenes_s = $_POST['imagenes_s'];
             $id_u = $_POST['id_u'];
             
-            $insertSalon = $salones_Obj->insert_Salon($nombre_s, $cap_personas_s, $estado_s, $imagenes_s, $id_u);
+            $insertsalones = $salones_Object->insert_salones($nombre_s, $cap_personas_s, $estado_s, $imagenes_s,  $id_u);
         } elseif (isset($_POST['update'])) {
-            // Comprobar si se hizo clic en el botón "Update"
-            // Obtener el ID del salón de eventos de la URL
-            if (isset($_GET['id'])) {
-                $id = $_GET['id'];
-                // Obtener los datos del formulario
-                $nombre_s = $_POST['Nombre_Salon'];
-                $cap_personas_s = $_POST['Capacidad_Personas'];
-                $estado_s = $_POST['Estado'];
-                $imagenes_s = $_POST['Imagenes'];
+            // Obtener el ID del usuario de la URL
+            $id = $_GET['id'];
+            // Obtener los datos del formulario
+            $nombre_s = $_POST['Nombre_s'];
+            $cap_personas_s = $_POST['cap_personas_s'];
+            $estado_s = $_POST['estado_s'];
+            $imagenes_s = $_POST['imagenes_s'];
+            $id_u = $_POST['id_u'];
                     
-                $updateSalon = $salones_Obj->update_Salon($id, $nombre_s, $cap_personas_s, $estado_s, $imagenes_s);
-            }
+            $updatesalones = $salones_Object->update_salones($id, $nombre_s, $cap_personas_s, $estado_s, $imagenes_s,  $id_u);
+
         } elseif (isset($_POST['delete'])) {
             // Comprobar si se hizo clic en el botón "Delete"
-            // Obtener el ID del salón de eventos de la URL
-            if (isset($_GET['id'])) {
-                $id = $_GET['id'];
-                $deleteSalon = $salones_Obj->delete_Salon($id);
-            }
+            // Obtener el ID del usuario de la URL
+            $id = $_GET['id'];
+                    
+            $deletesalones = $salones_Object->delete_salones($id);
         }
 
-        // Redirigir a la página de salones de eventos después de cualquiera de las operaciones (Create, Update o Delete)
-        header('Location: ../Salones/');
-        exit;
+        // Incluir la vista de usuarios después de cualquiera de las operaciones (Create, Update, o Delete)
+        echo "<script>location.href='../Salones/'</script>";
 
-?>
+    } catch (\Throwable $th) {
+        // En caso de que ocurra un error, redirecciona al usuario a la página de error interno.
+        echo "<script>location.href='../Error-Internal/'<script>";
+    }
+    
+} else {
+    // Si no existe una sesión activa o el valor de 'email' no está establecido, redirecciona al usuario a la página principal.
+    echo "<script>location.href='../'</script>";
+}
